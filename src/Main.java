f56f1941d243d40f7a4eeb7a6ab6d9dc07158a23
TTranchese <tommaso.tranchese.96@gmail.com>
2023-02-23 12:41:51 +0100

    fixed comments

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class Main {
 	public static void main(String[] args) {
 		String string1 = "United Kingdom";
 		String string2 = "Germany";
-
+		//trovo l'indice dello spazio
 		int spaceIndex = string1.indexOf(" ");
+		//dichiaro entrambe le stringhe e le assegno
 		String sub1 = string1.substring(0, spaceIndex);
 		String sub2 = string1.substring(spaceIndex+1);
-
+		//dichiaro e assegno il charArray
 		char[] charArray = (sub1 + " " + string2 + " " + sub2).toUpperCase().toCharArray();
 
-		// Print the char array
 		System.out.println(charArray);
 	}
 }
